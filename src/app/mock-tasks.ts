424a1db490cb9fd82b00e079ce67c052b8032019
alvalle09 <phredvalle@gmail.com>
2021-12-14 23:40:17 -0600

    add mock tasks

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Dr. Appointment',
+        day: 'May 5th, 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Study',
+        day: 'May 6th, 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Groceries',
+        day: 'May 6th, 2:30pm',
+        reminder: false,
+    },
+    {
+        id: 4,
+        text: 'School Meeting',
+        day: 'May 7th, 2:30pm',
+        reminder: false,
+    }
+]
