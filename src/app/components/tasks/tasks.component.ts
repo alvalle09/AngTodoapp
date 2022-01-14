8a8f895d9ef8691452b509b11598fb3de03f15f3
alvalle09 <phredvalle@gmail.com>
2022-01-13 23:57:21 -0600

    implement toggletask

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,8 +15,11 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
-    this.taskService.getTasks().subscribe((tasks) => 
-      this.tasks = tasks);
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+      console.log(tasks);
+    }
+      );
   }
 
   deleteTask(task: Task) {
