e5a223f54939df9619ea90c2f6e774e7b5e0b61c
alvalle09 <phredvalle@gmail.com>
2022-02-02 22:50:49 -0600

    setup add task with ngmodel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -21,7 +22,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
     // FontAwesomeModule
   ],
   providers: [],
