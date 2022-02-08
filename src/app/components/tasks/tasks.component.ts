1e0242c1b8cc4ae727d444fe80e0c4f00f1c5d7c
alvalle09 <phredvalle@gmail.com>
2022-02-07 23:06:32 -0600

    implement save task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -36,7 +36,8 @@ export class TasksComponent implements OnInit {
   }
 
   addTask(task: Task) {
-    console.log(task);
+    //console.log(task);
+    this.taskService.addTask(task).subscribe((task) => (this.tasks.push(task)));
   }
 
 }
